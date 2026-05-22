ba1019ae06c06510080cbd479e0352463ffffe17
Mohitsainix <msksaini20@gmail.com>
2026-05-22 11:11:41 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -55,9 +55,9 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function () {
   if (!this.isModified('password')) {
-    next();
+    return; // Just return, don't use next() in async Mongoose hooks
   }
 
   const salt = await bcrypt.genSalt(10);
